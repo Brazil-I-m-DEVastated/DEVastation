500a20b8b2172f92ad82ba0279d935ded667cdd4
rwsleal <rwsleal@gmail.com>
2023-03-27 16:01:24 -0300

    fix: db connection string

--- transactions/src/db/config/mongodb-connection.js
+++ transactions/src/db/config/mongodb-connection.js
@@ -2,7 +2,12 @@ import mongoose from 'mongoose';
 
 mongoose.set('runValidators', true);
 
-mongoose.connect('mongodb://admin:secret@${DB_HOST}:27017/devastation-transactions?authSource=admin');
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || '27017';
+
+mongoose.connect(
+    `mongodb://admin:secret@${DB_HOST}:${DB_PORT}/devastation-transactions?authSource=admin`
+);
 
 const db = mongoose.connection;
 
