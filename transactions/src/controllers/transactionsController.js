bf5d333c66e177a070d1883012c0cadc9ce03caf
rwsleal <rwsleal@gmail.com>
2023-03-27 19:53:29 -0300

    refactor: change sync methods to async

--- transactions/src/controllers/transactionsController.js
+++ transactions/src/controllers/transactionsController.js
@@ -1,43 +1,39 @@
 import Transaction from '../model/Transaction.js';
 
 class TransactionController {
-    static getById = (req, res, next) => {
+    static getById = async (req, res) => {
         const { id } = req.params;
-        Transaction.findOne({ id }, (err, transaction) => {
-            if (err) {
-                next(err);
-            } else {
-                res.status(200).json(transaction);
-            }
-        });
+        const transactionById = await Transaction.findById(id);
+
+        return res.status(200).json(transactionById);
+        
     };
 
     static create = async (req, res) => {
         const transaction = new Transaction(req.body);
-        try { await transaction.save();
-            return res.status(201).json(transaction); }
-        catch (err) { return res.status(500).send(err.message); }
+        await transaction.save();
+        
+        return res.status(201).json(transaction);
     };
 
-    static updateStatus = (req, res, next) => {
-        const { status } = req.body;
+    static updateStatus = async (req, res) => {
         const { id } = req.params;
-        const transactionById = Transaction.findOne({ id }, (err, transaction) => {
-            if(err) {
-                next(err);
-            } else {
-                return transaction;
-            }
-        });
+        const { status } = req.body;
 
-        if ( transactionById.status === 'EM ANÁLISE') {
-            Transaction.findOneAndUpdate({ id }, {$set: { status }}, (err, transaction) => {
-                if (err) {
-                    next(err);
-                } else {
-                    res.status(200).json(transaction);
-                }
-            });
+        const transaction = await Transaction.findById(id);
+        console.log(status);
+        console.log(transaction.status);
+
+
+        if ( transaction.status === 'Em Análise') {
+
+            const updatedTransaction = await Transaction
+                .findByIdAndUpdate(id, { status }, { new: true });
+
+            return res.status(200).json(updatedTransaction);
+           
+        } else { 
+            throw new Error('422|"transaction" status must be a valid one');
         }
     };
 }
