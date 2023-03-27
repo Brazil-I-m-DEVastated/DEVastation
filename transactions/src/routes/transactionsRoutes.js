bc594f3299b3a04c8cdef1f55afb561a3c525708
leticia <48692890+monteiroleticia@users.noreply.github.com>
2023-03-27 18:13:36 -0300

    fix: POST transactions

--- transactions/src/routes/transactionsRoutes.js
+++ transactions/src/routes/transactionsRoutes.js
@@ -6,8 +6,8 @@ import 'express-async-errors';
 const router = express.Router();
 
 router
-    .get('transactions/:id', TransactionController.getById)
-    .post('transactions', TransactionController.create)
-    .patch('transactions/:id', TransactionController.updateStatus);
+    .get('/transactions/:id', TransactionController.getById)
+    .post('/transactions', TransactionController.create)
+    .patch('/transactions/:id', TransactionController.updateStatus);
 
 export default router;
